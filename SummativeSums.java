dd1b938264ecf5a5062eb898e4b0c113118a07f5
kaluzy <kaluzyt@gmail.com>
2022-05-19 21:25:59 -0400

    Create a method to add multidimensional arrays

---
+++ SummativeSums.java
@@ -0,0 +1,28 @@
+
+
+public class SummativeSums {
+    public static void main(String[] args){
+        int[] array1 ={ 1, 90, -33, -55, 67, -16, 28, -55, 15 };
+        int[] array2 ={ 999, -60, -77, 14, 160, 301 };
+        int[] array3 ={ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130,
+                140, 150, 160, 170, 180, 190, 200, -99 };
+
+
+        System.out.println("#1 Array Sum: "+addArrays(array1));
+
+
+
+        System.out.println("#2 Array Sum: "+addArrays(array2));
+        System.out.println("#3 Array Sum: "+addArrays(array3));
+    }
+
+    public static int addArrays(int[] arr) {
+        int sum = 0;
+        for (int j : arr) {
+            sum += j;
+        }
+        return sum;
+    }
+}
+
+
